9dbd35d82d60449d836f3f50c5fc5b659c2ff390
wllacer <wllacer@gmail.com>
2018-04-10 14:15:05 +0200

    grand directory reshuffle (IV) base modules. Danacube

---
+++ danacube.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+from base.danacube import DanaCubeWindow,generaArgParser
+from PyQt5.QtWidgets import QApplication 
+
+if __name__ == '__main__':
+    import sys
+
+    # con utf-8, no lo recomiendan pero me funciona
+    #print(sys,version_info)
+    if sys.version_info[0] < 3:
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
+    
+    parser = generaArgParser()
+    args = parser.parse_args()
+
+    app = QApplication(sys.argv)
+    window = DanaCubeWindow(args)
+    window.resize(app.primaryScreen().availableSize().width(),app.primaryScreen().availableSize().height())
+    window.show()
+    sys.exit(app.exec_())
