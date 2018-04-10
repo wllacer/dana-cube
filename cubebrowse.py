2a1e1f5a2ae5eaa1d31a15c3ba6f82710ffced7d
wllacer <wllacer@gmail.com>
2018-04-10 11:34:05 +0200

    profundidad de presentacion inicial en cubebrowse a 0

--- cubebrowse.py
+++ cubebrowse.py
@@ -195,12 +195,14 @@ class CubeMgr(QTreeView):
         self.view.customContextMenuRequested.connect(self.openContextMenu)
         self.view.doubleClicked.connect(self.test)
         self.view.setModel(self.baseModel)
-        #self.view.hideColumn(2) # eso no interesa al usuario final
+        self.view.hideColumn(2) # eso no interesa al usuario final        
         self.view.expandAll() # es necesario para el resize
+
         for m in range(self.baseModel.columnCount()):
             self.view.resizeColumnToContents(m)
-        #self.view.collapseAll()
-        #self.view.expandToDepth(1)     
+        self.view.collapseAll()
+        #self.view.expandToDepth(0)     
+
         #self.view.verticalHeader().hide()
         #self.view.setHeaderHidden(True)
         #self.view.setSortingEnabled(True)
