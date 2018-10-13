70b116d715861d40d69303b39c81ea015fa1b174
wllacer <wllacer@gmail.com>
2018-10-13 22:35:50 +0200

    At last copyright notice at least in the support modules

--- support/datalayer/datemgr.py
+++ support/datalayer/datemgr.py
@@ -1,4 +1,14 @@
 # -*- coding=utf-8 -*-
+
+"""
+Part of  Global Common modules by Werner Llácer (c) 2012-2018
+
+As an integral part of a project distributed under an Open Source Licence, the licence of the proyect
+Used as  standalone module or outside the scope of  a project valid according to the  previous paragraph, or when  in doubt, distributed according to the terms of the GNU LGPL v2.0 license or higher numbered versions.
+The text of that particular version is available at https://www.gnu.org/licenses/old-licenses/lgpl-2.0.html
+
+"""
+
 from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
--- support/util/uf_menuEditor.py
+++ support/util/uf_menuEditor.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+Part of  Global Common modules by Werner Llácer (c) 2012-2018
 
+As an integral part of a project distributed under an Open Source Licence, the licence of the proyect
+Used as  standalone module or outside the scope of  a project valid according to the  previous paragraph, or when  in doubt, distributed according to the terms of the GNU LGPL v2.0 license or higher numbered versions.
+The text of that particular version is available at https://www.gnu.org/licenses/old-licenses/lgpl-2.0.html
 
+"""
+"""
 para validar contenido
 definir una señal validate 
 en el slot de validate
