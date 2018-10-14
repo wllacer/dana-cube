7afd1f2810ff6ab49a10985243a0b8af9a2c9cf5
wllacer <wllacer@gmail.com>
2018-10-14 02:23:44 +0200

    At last copyright notice in remainig py modules

--- user/basic.py
+++ user/basic.py
@@ -1,5 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+Part of  Dana-Cube Proyect by Werner Llácer (c) 2012-2018
+
+Distributed according to the terms of the GNU LGPL v2.0 license or higher numbered versions.
+The text of that particular version is available at https://www.gnu.org/licenses/old-licenses/lgpl-2.0.html
+Please see https://github.com/wllacer/dana-cube#license for further particulars about licencing of the Dana-Cube Project 
+
+"""
 '''
 Ver documentacion general de la api en ../docs/user_functions.md
 '''
